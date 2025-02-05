afd7e6dc207f3569bfe2985980e465eb62a1fd6b
lobes <samuel.t.giles@gmail.com>
2025-02-05 12:48:15 +0800

    apparently i didnt export module

--- build.zig
+++ build.zig
@@ -51,6 +51,9 @@ pub fn build(b: *std.Build) void {
         },
     });
 
+    // Export the module
+    b.modules.put("zdl", zdl_module) catch unreachable;
+
     const lib = b.addStaticLibrary(.{
         .name = "zdl",
         .root_source_file = .{ .cwd_relative = "zdl.zig" },
